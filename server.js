42e31c0eccc7b116e36ff5ec39d968963be35d21
duhaime <douglas.duhaime@gmail.com>
2020-11-05 20:20:06 -0500

    cleanup

--- server.js
+++ server.js
@@ -33,10 +33,6 @@ app.use(session({
 * Routes
 **/
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.post('/api/save_username', (req, res) => {
   // store the username to the session
   req.session.username = req.body.username;
@@ -53,8 +49,7 @@ app.get('/api/get', (req, res) => {
 app.post('/api/insert', (req, res) =>{
   const nameOne = req.body.name;
   const catChoiceOne = req.body.catchoice;
-  console.log(error);
-  const sqlInsert = 'INSERT INTO catshackdatabase (name, catchoice) VALUES (?,?)'
+  const sqlInsert = 'INSERT INTO catshackdatabase (name, catchoice) VALUES (?,?)';
   db.query(sqlInsert, {name, catchoice}, (err, result) =>{
     console.log(result);
   })
